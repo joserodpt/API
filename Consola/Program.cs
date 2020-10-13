827019540ff0cdb0e366a3340295cb85d8bdc994
José Rodrigues <josegamerpt@gmail.com>
2020-10-13 22:28:08 +0100

    Add project files.

---
+++ Consola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+        }
+    }
+}
