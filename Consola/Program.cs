e6d5203324e84f2ce7e60216fb0cb9431f21d4ad
José Rodrigues <josegamerpt@gmail.com>
2020-10-15 14:20:01 +0100

    Resolução exercício 1 ficha 7

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -6,7 +6,28 @@ namespace Consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!");
+            int a, b, c, triploa, produtobc;
+
+            log("Introduz os 3 números:");
+            a = int.Parse(Console.ReadLine());
+            log("a: " + a);
+            b = int.Parse(Console.ReadLine());
+            log("b: " + b);
+            c = int.Parse(Console.ReadLine());
+            log("c: " + c);
+            log("-+-+-+-+-+-+-+-+-");
+
+            triploa = a * 3;
+            produtobc = b * c;
+            double quadradoc = Math.Pow(c, 2);
+
+            log("Triplo de a: " + triploa + " | Produto de b com c: " + produtobc + " | Quadrado de C: " + quadradoc);
+            Console.ReadKey();
+        }
+
+        static void log(String s)
+        {
+            Console.WriteLine(s);
         }
     }
 }
