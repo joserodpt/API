92a102c251a36e3ff4274055cbf081d2c5db9e8e
José Rodrigues <josegamerpt@gmail.com>
2020-10-15 15:21:23 +0100

    resolução exercicio 6 ficha 6

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -6,18 +6,20 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            double raio, altura, volume, raio2;
+            double teste1, teste2, teste3, media;
 
-            log("Introduzir raio:");
-            raio = double.Parse(Console.ReadLine());
-            log("Introduzir altura:");
-            altura = double.Parse(Console.ReadLine());
+            //pedir notas dos 3 testes
+            log("Introduzir nota do teste 1:");
+            teste1 = double.Parse(Console.ReadLine());
+            log("Introduzir nota do teste 2:");
+            teste2 = double.Parse(Console.ReadLine());
+            log("Introduzir nota do teste 3:");
+            teste3 = double.Parse(Console.ReadLine());
 
-            //calcular raio^2
-            raio2 = Math.Pow(raio, 2.00);
-            volume = 3.2222 * raio2 * altura;
+            //calcular media
+            media = (teste1 + teste2 + teste3) / 3;
            
-            log("O volume de raio " + raio + " e de altura " + altura + " é: " + volume);
+            log("A média dos 3 testes é de: " + media + " valores.");
             Console.ReadKey();
         }
 
