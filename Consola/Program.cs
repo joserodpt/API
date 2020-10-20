e115ad2981fd4e3605f0e4bef6fe3639a29710c1
José Rodrigues <josegamerpt@gmail.com>
2020-10-20 09:27:07 +0100

    adicionado Math.Round ao exercício

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -14,7 +14,7 @@ namespace Consola
             escudos = euros * taxa_de_conversao;
             log("-+-+-+-+-+-+-+-+-");
 
-            log("O valor de " + euros + " euros em escudos é: " + escudos);
+            log("O valor de " + euros + " euros em escudos é: " + Math.Round(escudos, 2));
             Console.ReadKey();
         }
 
