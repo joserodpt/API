c0ab567cd20d456ba83f4fa40ce7fdee09895588
José Rodrigues <josegamerpt@gmail.com>
2020-10-20 08:46:40 +0100

    resolução ficha 7 exer 7

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -6,20 +6,18 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            double teste1, teste2, teste3, media;
+            double classific_freq, classific_exame, resultado;
 
-            //pedir notas dos 3 testes
-            log("Introduzir nota do teste 1:");
-            teste1 = double.Parse(Console.ReadLine());
-            log("Introduzir nota do teste 2:");
-            teste2 = double.Parse(Console.ReadLine());
-            log("Introduzir nota do teste 3:");
-            teste3 = double.Parse(Console.ReadLine());
+            //pedir classificações
+            log("Introduzir Classificação da Frequência:");
+            classific_freq = double.Parse(Console.ReadLine());
+            log("Introduzir Classificação do Exame:");
+            classific_exame = double.Parse(Console.ReadLine());
 
             //calcular media
-            media = (teste1 + teste2 + teste3) / 3;
+            resultado = classific_freq*0.75 + classific_exame*0.25;
            
-            log("A média dos 3 testes é de: " + media + " valores.");
+            log("A classificação final é de: " + resultado + " valores.");
             Console.ReadKey();
         }
 
