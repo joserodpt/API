dea3363687bb9b9f4858a4fabc10fe6116bb334e
José Rodrigues <josegamerpt@gmail.com>
2020-10-20 15:29:22 +0100

    pequena correção

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -6,20 +6,20 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            double teste1, teste2, teste3, media;
+            Double teste1, teste2, teste3, media;
 
             //pedir notas dos 3 testes
             log("Introduzir nota do teste 1:");
-            teste1 = double.Parse(Console.ReadLine());
+            teste1 = Double.Parse(Console.ReadLine());
             log("Introduzir nota do teste 2:");
-            teste2 = double.Parse(Console.ReadLine());
+            teste2 = Double.Parse(Console.ReadLine());
             log("Introduzir nota do teste 3:");
-            teste3 = double.Parse(Console.ReadLine());
+            teste3 = Double.Parse(Console.ReadLine());
 
             //calcular media
             media = (teste1 + teste2 + teste3) / 3;
            
-            log("A média dos 3 testes é de: " + media + " valores.");
+            log("A média dos 3 testes é de: " + Math.Round(media, 2) + " valores.");
             Console.ReadKey();
         }
 
