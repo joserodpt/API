2e3550445bef873b5715b4b67fff3c0c4a7aebf6
José Rodrigues <josegamerpt@gmail.com>
2020-10-22 15:00:25 +0100

    resolução ficha 8 exer 3

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -13,13 +13,23 @@ namespace Consola
             log("Introduzir b:");
             b = Double.Parse(Console.ReadLine());
 
-            if (a > b)
+            if (a == b)
             {
-                log("a > b. a: " + a);
-            } else
-            {
-                log("a < b. b: " + b);
+                log("a e b são iguais.");
+                Console.ReadKey();
             }
+            else
+            {
+                if (a > b)
+                {
+                    log("a > b. a: " + a);
+                }
+                else
+                {
+                    log("a < b. b: " + b);
+                }
+            }
+            Console.ReadKey();
         }
 
         static void log(String s)
