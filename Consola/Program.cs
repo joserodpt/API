985ca56b263ee9c373b636fccdb6174525c9c31b
José Rodrigues <josegamerpt@gmail.com>
2020-10-22 15:26:43 +0100

    ficha 8 exer 7

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -7,30 +7,26 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            Double a, b;
+            Double lado1, lado2;
 
-            log("Introduzir a:");
-            a = Double.Parse(Console.ReadLine());
-            log("Introduzir b:");
-            b = Double.Parse(Console.ReadLine());
+            log("Introduzir lado1:");
+            lado1 = Double.Parse(Console.ReadLine());
+            log("Introduzir lado2:");
+            lado2 = Double.Parse(Console.ReadLine());
 
-            if (a == b)
+            if (lado1 == lado2)
             {
-                log("a e b são iguais.");
-                Console.ReadKey();
-                return;
-            }
-
-            if (a > b)
+                //triangulo
+                double areat = (lado1*lado2)/ 2;
+                log("Área do triângulo: " + areat);
+            } else
             {
-                log("a > b. a: " + a);
-            }
-            else
-            {
-                log("a < b. b: " + b);
+                //retangulo
+                log("Área do retângulo: " + lado1*lado2);
             }
 
             Console.ReadKey();
+
         }
 
         static void log(String s)
