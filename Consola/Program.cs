d66d018bc5c9b8f0062454c2f72c4167cc1077f1
José Rodrigues <josegamerpt@gmail.com>
2020-10-27 09:16:59 +0000

    ~.

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -13,26 +13,27 @@ namespace Consola
             switch(dia)
             {
                 case 1:
-                    log("É segunda feira!");
-                    break;
-                case 2:
-                    log("É terça feira!");
-                    break;
-                case 3:
-                    log("É quarta feira!");
-                    break;
-                case 4:
-                    log("É quinta feira!");
-                    break;
-                case 5:
-                    log("É sexta feira!");
-                    break;
-                case 6:
-                    log("É sábado!");
-                    break;
-                case 7:
                     log("É domingo!");
                     break;
+                case 2:
+                    log("É segunda feira!");
+                    break;
+                case 3:
+                    log("É terça feira!");
+                    break;
+                case 4:
+                    log("É quarta feira!");
+                    break;
+                case 5:
+                    log("É quinta feira!");
+                    break;
+                case 6:
+                    log("É sexta feira!");
+                    break;
+                case 7:
+                    log("É sábado!");
+                    break;
+             
             }
 
 
