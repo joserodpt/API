09e1dc83634faf23eb6fc6f47cfe8da102d92c93
José Rodrigues <josegamerpt@gmail.com>
2020-10-27 14:57:30 +0000

    solução correta com IFs

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -5,22 +5,26 @@ using System.Linq;
 namespace Consola
 {
     class Program {
-    
+
+        static Double top;
+
         static void Main(string[] args)
         {
-            List<Double> lista = new List<Double>();
 
             log("Introduzir 1º número:");
-            lista.Add(Double.Parse(Console.ReadLine()));
+            verificar(Double.Parse(Console.ReadLine()));
             log("Introduzir 2º número:");
-            lista.Add(Double.Parse(Console.ReadLine()));
+            verificar(Double.Parse(Console.ReadLine()));
             log("Introduzir 3º número:");
-            lista.Add(Double.Parse(Console.ReadLine()));
+            verificar(Double.Parse(Console.ReadLine()));
 
-            Double max = lista.Max();
-
-            log("O maior número têm o valor " + max);
+            log("O maior número têm o valor " + top);
             Console.ReadKey();
+        }
+
+        static void verificar(double v)
+        {
+            if (v > top) { top = v; }
         }
 
         static void log(String s)
