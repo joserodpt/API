150d437f32e6a5f13393814548351252db1ad42a
José Rodrigues <josegamerpt@gmail.com>
2020-10-27 09:15:08 +0000

    ficha 9 exer 2

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -8,33 +8,41 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            log("Introduzir dia da semana:");
-            int dia = int.Parse(Console.ReadLine());
-            switch(dia)
+            Double a, b;
+            int escolha;
+
+            log("~ Calculadora Simplex ~");
+            log("Introduzir número1:");
+            a = Double.Parse(Console.ReadLine());
+            log("Introduzir número2:");
+            b = Double.Parse(Console.ReadLine());
+            log("Operações possíveis:" +
+                "\n1 - Somar" +
+                "\n2 - Subtrair" +
+                "\n3 - Multiplicar" +
+                "\n4 - Dividir" +
+                "\n(Digita a opção pretendida)");
+            escolha = int.Parse(Console.ReadLine());
+
+        
+            switch(escolha)
             {
                 case 1:
-                    log("É segunda feira!");
+                    log("A soma dá: " + (a+b));
                     break;
                 case 2:
-                    log("É terça feira!");
+                    log("A subtração dá: " + (a - b));
                     break;
                 case 3:
-                    log("É quarta feira!");
+                    log("A multiplicação dá: " + (a * b));
                     break;
                 case 4:
-                    log("É quinta feira!");
+                    log("A divisão dá: " + (a/b));
                     break;
-                case 5:
-                    log("É sexta feira!");
-                    break;
-                case 6:
-                    log("É sábado!");
-                    break;
-                case 7:
-                    log("É domingo!");
+                default:
+                    log("Opção não reconhecida.");
                     break;
             }
-
 
             Console.ReadKey();
         }
