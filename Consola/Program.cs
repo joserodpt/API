291a13229b80088f5d8f468ed31f626952d08768
José Rodrigues <josegamerpt@gmail.com>
2020-11-03 09:17:39 +0000

    ficha 10 exer 2

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -8,16 +8,8 @@ namespace Consola
     
         static void Main(string[] args)
         {
-            double soma, media;
-            soma = 0;
-            log("Introduza o número N:");
-            int n = int.Parse(Console.ReadLine());
-            for (int i=1; i<=n; i++)
-            {
-                soma = soma + i;
-            }
-            media = soma / n;
-            log("A média de " + n + " números inteiros é: " + media);
+            for (int i = 1; i <= 15; i++)
+                log(i + "");
 
             Console.ReadKey();
         }
