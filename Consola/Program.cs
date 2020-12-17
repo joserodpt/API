64f439e1cf2217200f673687086ba789c74569ec
José Rodrigues <josegamerpt@gmail.com>
2020-12-17 15:23:34 +0000

    exercício 13 com Console.SetCursorPosition

--- Consola/Program.cs
+++ Consola/Program.cs
@@ -5,21 +5,26 @@ using System.Linq;
 namespace Consola
 {
     class Program {
-        static int lado;
+        static int lado, posx, posy;
         static String icon = "■";
         static void Main(string[] args)
         {
             log("Introduzir lado:");
             lado = int.Parse(Console.ReadLine());
+            log("Introduzir PosX:");
+            posx = int.Parse(Console.ReadLine());
+            log("Introduzir PosY:");
+            posy = int.Parse(Console.ReadLine());
 
             if (lado > 0)
             {
                 if (lado > 1)
                 {
                     lado = lado - 2;
-                    quadrad();
+                    quadrado();
                 } else
                 {
+                    Console.SetCursorPosition(posx, posy);
                     log(icon);
                 }
             } else
@@ -29,28 +34,42 @@ namespace Consola
             Console.ReadKey();
         }
 
-        static void quadrad()
+        static void quadrado()
         {
-            topobaixo();
+            topo();
             bordas();
-            topobaixo();
+            baixo();
         }
 
-        static void topobaixo() {
+        static void topo() {
+            Console.SetCursorPosition(posx, posy);
             String print = "";
             for (int a = 0; a <= lado + 1; a++) { print += icon + " "; }
             log(print);
         }
+
+        static void baixo()
+        {
+            Console.SetCursorPosition(posx, posy + lado + 1);
+            String print = "";
+            for (int a = 0; a <= lado + 1; a++) { print += icon + " "; }
+            log(print);
+        }
+
         static void bordas() {
-            for(int lop = 0; lop < lado; lop++) {
+
+            for (int lop = 0; lop <= lado; lop++) {
+
+                Console.SetCursorPosition(posx, posy + (lop + 1));
+
+
                 String print = icon;
                 for (int a = 0; a <= lado * 2; a++) { print += " "; }
                 print += icon;
                 log(print);
             }
         }
-
-
+        
         static void log(String s) {  Console.WriteLine(s);  }
     }
 }
